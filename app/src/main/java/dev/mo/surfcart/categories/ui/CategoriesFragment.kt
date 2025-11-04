bbe91bbf2b250be3f80ee527e82955303fb29bd5
mohamedshemees <mohamedessampd@gmail.com>
2025-11-04 18:30:21 +0200

    add : shimmer effect for category screen

--- app/src/main/java/dev/mo/surfcart/categories/ui/CategoriesFragment.kt
+++ app/src/main/java/dev/mo/surfcart/categories/ui/CategoriesFragment.kt
@@ -16,9 +16,11 @@ import com.google.android.material.carousel.CarouselSnapHelper
 import dagger.hilt.android.AndroidEntryPoint
 import dev.mo.surfcart.databinding.FragmentCategoriesBinding
 import dev.mo.surfcart.home.ui.CategoryAdapter
+import dev.mo.surfcart.home.ui.CategoryShimmerAdapter
 import dev.mo.surfcart.product_details.ui.ProductDetailsFragmentDirections
 import dev.mo.surfcart.products.ui.ChildCategoryFragmentArgs
 import dev.mo.surfcart.products.ui.ProductAdapter
+import dev.mo.surfcart.products.ui.ProductShimmerAdapter
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -33,6 +35,8 @@ class CategoriesFragment : Fragment() {
     private lateinit var snapHelper: CarouselSnapHelper
     private lateinit var subCategoriesAdapter: CategoryAdapter
     private lateinit var productAdapter: ProductAdapter
+    private lateinit var productShimmerAdapter: ProductShimmerAdapter
+    private lateinit var categoryShimmerAdapter: CategoryShimmerAdapter
 
 
     override fun onCreateView(
@@ -47,25 +51,46 @@ class CategoriesFragment : Fragment() {
         val args: ChildCategoryFragmentArgs by navArgs()
         val categoryId = args.categoryId
 
+        initViews()
+
+
+        binding.carouselShimmerLayout.startShimmer()
+        binding.categoryShimmerLayout.startShimmer()
+        binding.productShimmerLayout.startShimmer()
+        
         categoriesViewModel.loadInitial(categoryId)
-        viewLifecycleOwner.lifecycleScope.launch {
-            initViews()
-        }
+
         viewLifecycleOwner.lifecycleScope.launch {
             categoriesViewModel.parentCategories.collect { categories ->
+                if (categories.isNotEmpty()) {
+                    binding.carouselShimmerLayout.stopShimmer()
+                    binding.carouselShimmerLayout.visibility = View.GONE
+                    binding.carouselRecyclerView.visibility = View.VISIBLE
+                }
                 carouselAdapter.submitList(categories)
-                carouselRecyclerView.scrollToPosition(
-                    categories.indexOfFirst { it.id.toLong() == categoryId }
-                )
+                val position = categories.indexOfFirst { it.id.toLong() == categoryId }
+                if (position != -1) {
+                    carouselRecyclerView.scrollToPosition(position)
+                }
             }
         }
         viewLifecycleOwner.lifecycleScope.launch {
             categoriesViewModel.subCategories.collect { subCategories ->
+                if (subCategories.isNotEmpty()){
+                    binding.categoryShimmerLayout.stopShimmer()
+                    binding.categoryShimmerLayout.visibility = View.GONE
+                    binding.categoryRv.visibility = View.VISIBLE
+                }
                 subCategoriesAdapter.submitList(subCategories)
             }
         }
         viewLifecycleOwner.lifecycleScope.launch {
             categoriesViewModel.products.collect { products ->
+                if(products.isNotEmpty()){
+                    binding.productShimmerLayout.stopShimmer()
+                    binding.productShimmerLayout.visibility = View.GONE
+                    binding.productsRv.visibility = View.VISIBLE
+                }
                 productAdapter.submitList(products)
             }
         }
@@ -102,6 +127,19 @@ class CategoriesFragment : Fragment() {
         binding.productsRv.adapter = productAdapter
         binding.productsRv.layoutManager =
             GridLayoutManager(requireContext(), 2, GridLayoutManager.HORIZONTAL, false)
+
+        // Shimmer Adapters
+        productShimmerAdapter = ProductShimmerAdapter()
+        binding.productShimmerRv.adapter = productShimmerAdapter
+        binding.productShimmerRv.layoutManager =
+            GridLayoutManager(requireContext(), 2, GridLayoutManager.HORIZONTAL, false)
+
+
+        categoryShimmerAdapter = CategoryShimmerAdapter()
+        binding.categoryShimmerRv.adapter = categoryShimmerAdapter
+        binding.categoryShimmerRv.layoutManager =
+            GridLayoutManager(requireContext(), 2, GridLayoutManager.HORIZONTAL, false)
+
     }
 
 }
